8920b9c7c884a4f48077a2ce622142bf51e66207
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-04-13 18:56:23 +0200

    finished first exercise

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Train;
+
+class PageController extends Controller
+{
+    public function homepage(){
+        $trains = Train::all();
+
+        $data = [
+            'trains' => $trains
+        ];
+
+        return view('welcome', $data);
+    }
+}
